12244a094bb068fe5beb7a96cf0c0335d6ce6779
Kiptanui Boaz <boaserem022@gmail.com>
2023-10-21 15:50:53 +0300

    added tuples

---
+++ annotations/arrays.ts
@@ -0,0 +1,7 @@
+const carMakers: string[] = ["ford", "chevy", "audi"]
+const carsByMake: string[][] = [];
+
+//help with inference when extractin values
+const carfqf = carMakers[2];
+
+carMakers.map((car) => car)
